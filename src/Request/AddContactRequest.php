9420ef7c6e24cd63f0ac5a20f815f416e7c6d4a2
Estratos Consulting <julio@estratos.net>
2026-06-29 00:35:11 -0600

    Create AddContactRequest.php

---
+++ src/Request/AddContactRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Estratos\DomainApiInterface\Request;
+
+final readonly class AddContactRequest
+{
+    public function __construct(
+        public string $firstName,
+        public string $lastName,
+        public string $email,
+        public string $phone,
+        public string $organization,
+        public string $address1,
+        public string $city,
+        public string $state,
+        public string $postalCode,
+        public string $country,
+        public ?string $address2 = null,
+        public ?string $fax = null,
+        public bool $isDefault = false,
+    ) {
+        if (empty($firstName)) {
+            throw new \InvalidArgumentException('First name cannot be empty');
+        }
+
+        if (empty($lastName)) {
+            throw new \InvalidArgumentException('Last name cannot be empty');
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException('Invalid email format');
+        }
+
+        if (empty($phone)) {
+            throw new \InvalidArgumentException('Phone cannot be empty');
+        }
+
+        if (empty($address1)) {
+            throw new \InvalidArgumentException('Address cannot be empty');
+        }
+
+        if (empty($city)) {
+            throw new \InvalidArgumentException('City cannot be empty');
+        }
+
+        if (empty($state)) {
+            throw new \InvalidArgumentException('State cannot be empty');
+        }
+
+        if (empty($postalCode)) {
+            throw new \InvalidArgumentException('Postal code cannot be empty');
+        }
+
+        if (empty($country)) {
+            throw new \InvalidArgumentException('Country cannot be empty');
+        }
+    }
+}
